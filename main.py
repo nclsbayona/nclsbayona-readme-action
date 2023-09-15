77ab9669bd5b2cf7bcf763678962e3be3b2f5230
nclsbayona <59931437+nclsbayona@users.noreply.github.com>
2023-09-14 22:00:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ async def getAffirmation() -> Dict[str, str]:
     except Exception or KeyboardInterrupt:
         return {
             "text_affirmation1": "An error ocurred", 
-            "text_affirmation2": "Please try again later"
+            "text_affirmation2": "Please try again later",
         }
 
 
